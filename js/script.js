852aff544cfa54a94283fe69b55d8cb226b43301
Anani Samuel <ananisamuelhope@gmail.com>
2023-08-18 21:05:22 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+var fullVid = document.getElementById("fullVid");
+
+function openFullImg(pic){
+    fullImgBox.style.display = "flex";
+    fullVid.style.display = "none";
+    fullImg.style.display = "block";
+    fullImg.src = pic;
+}
+
+function openFullVid(vid){
+    fullImgBox.style.display = "flex";
+    fullImg.style.display = "none";
+    fullVid.style.display = "block";
+    fullVid.src = vid;
+}
+
+function closeFullImg(){
+    fullImgBox.style.display = "none";
+}
